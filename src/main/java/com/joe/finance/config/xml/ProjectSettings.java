b44bffb056e67798b229fe66e3f3cab414f551ee
Joe Ting <jcting@yahoo.com>
2016-04-27 20:45:05 -0700

    removing target directory

--- src/main/java/com/joe/finance/config/xml/ProjectSettings.java
+++ src/main/java/com/joe/finance/config/xml/ProjectSettings.java
@@ -2,6 +2,6 @@ package com.joe.finance.config.xml;
 
 public class ProjectSettings {
 	public static final String CONFIG_LOCATION_PREFIX = 
-			"/Users/IPL/git/joe";
-	public static final String FUND_NAME = "dave";
+			"/Users/joe/Documents/workspace/finance/";
+	public static final String FUND_NAME = "joe";
 }
